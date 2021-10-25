40f40e3f34d5a3c2f8a1030dbc47b7027cf8fedc
sirin0762 <tlsdydwls123@gmail,com>
2021-10-25 13:27:35 +0900

    FEAT : 초기 세팅

---
+++ src/main/java/com/programmers/yogijogi/YogiJogiApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.yogijogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogiJogiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YogiJogiApplication.class, args);
+    }
+
+}
