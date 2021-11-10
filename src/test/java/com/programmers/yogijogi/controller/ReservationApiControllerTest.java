6ad96e79d89fb88f487c3e3e63033c08feea74b3
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2021-11-10 14:48:01 +0900

    Refactor. 주석 수정

--- src/test/java/com/programmers/yogijogi/controller/ReservationApiControllerTest.java
+++ src/test/java/com/programmers/yogijogi/controller/ReservationApiControllerTest.java
@@ -33,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+//
 public class ReservationApiControllerTest {
 
     @Autowired
